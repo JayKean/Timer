de1223a889a4793f2a39de759fc82c8e76bc03d9
JayKean <62370359+JayKean@users.noreply.github.com>
2021-07-07 16:01:53 +1200

    added css styling, completed timer works perfectly

--- index.js
+++ index.js
@@ -1,32 +1,24 @@
-class Timer{
-    constructor(durationInput, startButton, pauseButton){
-            
-            this.durationInput = durationInput;
-            this.startButton = startButton;
-            this.pauseButton = pauseButton;
-
-            this.startButton.addEventListener('click', this.start)
-            this.pauseButton.addEventListener('click', this.pause)
-    }
-
-start = () => {
-    this.tick();
-    this.interval = setInterval(this.tick, 1000);
-    };
-
-pause = () => {
-    clearInterval(this.interval);
-};
-
-tick = () => {
-        const timeRemaining = parseFloat(this.durationInput.value);
-        this.durationInput.value = timeRemaining - 1;
-    };
-}
-
 const durationInput = document.querySelector('#duration');
 const startButton = document.querySelector('#start');
 const pauseButton = document.querySelector('#pause');
+const circle = document.querySelector('circle');
 
-const timer = new Timer(durationInput, startButton, pauseButton);
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
 
+let duration;
+let currentOffset = 0;
+
+const timer = new Timer(durationInput, startButton, pauseButton, {
+    onStart(totalDuration) {
+      duration = totalDuration;
+    },
+    onTick(timeRemaining) {
+        circle.setAttribute('stroke-dashoffset',
+        perimeter * timeRemaining / duration - perimeter);
+    },
+    onComplete() {
+        console.log('Timer is completed')
+    }
+  });
+  
